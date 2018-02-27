f7c39db1eeaa4ec0717a30c050391f36c40eb725
xana8u <xana8u@gmail.com>
2018-02-27 16:58:58 +0200

    +made functions to simplify -fixed error where having towermoves first element be towers[0] the entire towers list mutilated, by making to creation loop add the same elements to both instead of using towers as content.
    todo: fix number placements to fit the rules

--- towerOfHanoi.py
+++ towerOfHanoi.py
@@ -14,17 +14,105 @@ import time
 
 disks = input("How many disks does the tower have?")
 towers = [[], [], []]
+towermoves = [[], [], []]
+lastmove = []
 
-for disk in range(1, int(disks)+1):
-    towers[0].append(disk)
+def createlist():
+    for disk in range(1, int(disks)+1):
+        towers[0].append(disk)
+        towermoves[0].append(disk)
 
-towermoves = [towers[0], [], []]
-print(towermoves[0:])
-
-while towermoves[2] != towers[0]:
-    if towermoves[2][0:1] < towermoves[0][0:1]:
-        print(1)
-
-    time.sleep(1)
+def isdone():
+    if towers[0] == towermoves[2]:
+        print("Done!", towermoves)
+        input("Program will exit on input:")
+        exit()
 
 
+def solve():
+    while True:
+        print(towers[0], "this")
+        """if not towermoves[2]:
+            if towermoves[2][0:1] < towermoves[0][0:1]:
+                towermoves[2].insert(0, towermoves[0][0:1][0])
+                del towermoves[0][0:1]
+                lastmove.append("02")
+                if len(lastmove) >= 2:
+                    del lastmove[0]
+                print(towermoves, "1")
+    
+        if not towermoves[1] and "01" not in lastmove:
+            if towermoves[1][0:1] < towermoves[0][0:1]:
+                towermoves[1].insert(0, towermoves[0][0:1][0])
+                del towermoves[0][0:1]
+                lastmove.append("01")
+                if len(lastmove) >= 2:
+                    del lastmove[0]
+                print(towermoves, "2")
+    
+        if not towermoves[0] and "20" not in lastmove:
+            if towermoves[0][0:1] < towermoves[2][0:1]:
+                towermoves[0].insert(0, towermoves[2][0:1][0])
+                del towermoves[2][0:1]
+                lastmove.append("20")
+                if len(lastmove) >= 2:
+                    del lastmove[0]
+                print(towermoves, "3")
+    """
+        if towermoves[2][0:1] < towermoves[0][0:1] and "02" not in lastmove:
+            towermoves[2].insert(len(towermoves[2]), towermoves[0][0:1][0])
+            del towermoves[0][0:1]
+            lastmove.append("02")
+            if len(lastmove) >= 2:
+                del lastmove[0]
+            print(towermoves, "4")
+            isdone()
+
+        if towermoves[1][0:1] < towermoves[0][0:1] and "01" not in lastmove:
+            towermoves[1].insert(len(towermoves[1]), towermoves[0][0:1][0])
+            del towermoves[0][0:1]
+            lastmove.append("01")
+            if len(lastmove) >= 2:
+                del lastmove[0]
+            print(towermoves, "5")
+            isdone()
+
+        if towermoves[0][0:1] < towermoves[1][0:1] and "10" not in lastmove:
+            towermoves[0].insert(len(towermoves[0]), towermoves[1][0:1][0])
+            del towermoves[1][0:1]
+            lastmove.append("10")
+            if len(lastmove) >= 2:
+                del lastmove[0]
+            print(towermoves, "6")
+            isdone()
+
+        if towermoves[0][0:1] < towermoves[2][0:1] and "20" not in lastmove:
+            towermoves[0].insert(len(towermoves[0]), towermoves[2][0:1][0])
+            del towermoves[2][0:1]
+            lastmove.append("20")
+            if len(lastmove) >= 2:
+                del lastmove[0]
+            print(towermoves, "7")
+            isdone()
+
+        if towermoves[1][0:1] < towermoves[2][0:1] and "21" not in lastmove:
+            towermoves[1].insert(len(towermoves[1]), towermoves[2][0:1][0])
+            del towermoves[2][0:1]
+            lastmove.append("21")
+            if len(lastmove) >= 2:
+                del lastmove[0]
+            print(towermoves, "8")
+            isdone()
+
+        if towermoves[2][0:1] < towermoves[1][0:1] and "12" not in lastmove:
+            towermoves[2].insert(len(towermoves[2]), towermoves[1][0:1][0])
+            del towermoves[1][0:1]
+            lastmove.append("12")
+            if len(lastmove) >= 2:
+                del lastmove[0]
+            print(towermoves, "9")
+            isdone()
+        time.sleep(1)
+
+createlist()
+solve()
